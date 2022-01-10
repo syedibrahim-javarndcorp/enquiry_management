48c55da674f620a9344966581061df71c900f1bf
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-01-10 16:21:35 -0500

    user data rendering

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 # Create your views here.
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profile = Profile.objects.all()
+    context = {
+        'profiles': profile
+    }
+    return render(request, 'users/profiles.html', context)
+
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+
+    return render(request, 'users/user-profiles.html', context)
