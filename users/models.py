05f55934dcecba1b11aa53ed2129db4ea7ab73e9
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-01-10 17:59:08 -0500

    user data rendering

--- users/models.py
+++ users/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 
 # Create your models here.
+
+
 class Profile(models.Model):
     USER_TYPE = (
         ('student', 'Student'),
@@ -18,4 +22,6 @@ class Profile(models.Model):
     id = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return str(self.username)
+        return str(self.user.username)
+
+
---
+++ users/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+@receiver(signal=post_save, sender=User)
+def profile_created(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(user=user,
+                                         username=user.username,
+                                         email=user.email,
+                                         name=user.first_name,
+                                         )
+
+
+@receiver(signal=post_delete, sender=Profile)
+def delete_user(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+# post_save.connect(profile_created, sender=User)
+# post_delete.connect(delete_user, sender=Profile)
