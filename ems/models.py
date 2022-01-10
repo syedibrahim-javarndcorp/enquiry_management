6dfc398f36368118e46fe4d245aa2a617175e92e
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-01-10 15:18:52 -0500

    user relation created

--- ems/models.py
+++ ems/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
+from users.models import Profile
 
 # Create your models here.
 class Ems(models.Model):
+    owner = models.ForeignKey(Profile,null=True,blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100, null=True, blank=True)
     title = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     tag = models.ManyToManyField('Tag', blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.AutoField(primary_key=True )
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +24,7 @@ class Review(models.Model):
     # owner =
     project = models.ForeignKey(Ems, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
-    value = models.CharField(max_length=150, choices= VOTE_TYPE)
+    value = models.CharField(max_length=150, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
     id = models.AutoField(primary_key=True)
 
@@ -38,4 +39,3 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
--- enquiry_management/settings.py
+++ enquiry_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'ems.apps.EmsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
