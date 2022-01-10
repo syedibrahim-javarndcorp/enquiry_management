7a780f7888c6d363dc2090f9aa1c6923f00747e8
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-01-10 09:19:18 -0500

    add and edit enquiry completed

--- ems/urls.py
+++ ems/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('id/<str:pk>', views.single_tag),
+    path('ems/<str:pk>/', views.single_tag, name='enquiry'),
+    path('add-enquiry/', views.create_enquiry, name='create-enquiry'),
+    path('update-enquiry/<str:pk>',views.update_enquiry, name='update-enquiry'),
 ]
--- ems/views.py
+++ ems/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ems
+from .forms import EmsForm
 
 
 # Create your views here.
@@ -18,3 +19,32 @@ def single_tag(request, pk):
         'tags': tags
     }
     return render(request, 'ems/profile_details.html', context)
+
+
+def create_enquiry(request):
+    form = EmsForm()
+
+    if request.method == 'POST':
+        form = EmsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {'form': form}
+    return render(request, 'ems/add-update.html', context)
+
+
+def update_enquiry(request, pk):
+    enquiry = Ems.objects.get(id=pk)
+    form = EmsForm(instance=enquiry)
+
+    if request.method == 'POST':
+        form = EmsForm(request.POST, instance=enquiry)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'ems/add-update.html', context)
