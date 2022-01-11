3305a2e19fdbd210ccc49b278f69bf5cd1536c1f
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-01-11 10:28:41 -0500

    Registration page created

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>', views.user_profile, name='user-profile'),
     path('login/', views.login_page, name='login'),
-    path('logout/',views.logout_page, name='logout'),
+    path('logout/', views.logout_page, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserForm
 
 
 # Create your views here.
@@ -34,6 +36,25 @@ def login_page(request):
 def logout_page(request):
     logout(request)
     return redirect('login')
+
+
+def register_user(request):
+    page = 'register'
+    form = CustomUserForm()
+
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('index')
+
+    context = {'page': page,
+               'form': form,
+               }
+    return render(request, 'users/login-register.html', context)
 
 
 def profiles(request):
